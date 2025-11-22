b51e52072044d7fb82622ec89e3d6bb80d1bf335
Ina Litvinka <inlibitum@gmail.com>
2025-11-22 22:41:59 +0100

    chore: add react-router, styled-components, tailwindcss packages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@tasks': path.resolve(__dirname, './src/tasks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    }
+  }
 });
