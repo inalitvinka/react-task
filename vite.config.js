f124612ee0b503226f9d4287ec9a611f07d8e92c
Ina Litvinka <inlibitum@gmail.com>
2025-11-25 14:02:01 +0100

    chore: update path aliases - add pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -9,7 +10,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@tasks': path.resolve(__dirname, './src/tasks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
       '@utils': path.resolve(__dirname, './src/utils'),
-    }
-  }
+    },
+  },
 });
