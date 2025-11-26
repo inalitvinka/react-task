8c74819bdabab186a853c93c6e87bdc39636fad4
Ina Litvinka <inlibitum@gmail.com>
2025-11-26 22:54:15 +0100

    chore: update base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'react-task',
+  base: '/react-task',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
